2020508acdd4bf9ae5d9ee6b58c84203a8f0c1f1
Mohammed <fromlatonyy@gmail.com>
2023-02-17 04:16:07 -0800

    er

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	char c[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 26; i > 0; i--)
+	{
+		putchar(c[i]);
+	}
+	putchar('\n');
+	return (0);
+}
