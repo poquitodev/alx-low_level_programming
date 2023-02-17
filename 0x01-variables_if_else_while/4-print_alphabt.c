66368d7a99baf95af4b5e7f03f796a4aeda1702b
Mohammed <fromlatonyy@gmail.com>
2023-02-17 03:41:18 -0800

    er

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	char c[26] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 26; i++)
+	{
+		if (c[i] != 'q' || c[i] != 'e')
+		{
+			putchar(c[i]);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
