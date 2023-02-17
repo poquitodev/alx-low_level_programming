19fcdde21ad30f5c4f3e937044bc3f5ac4abc87a
Mohammed <fromlatonyy@gmail.com>
2023-02-17 03:48:47 -0800

    er

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	int n = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
