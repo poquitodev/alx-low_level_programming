e65b71229110ff4a3047319a4b96f606ba242205
Mohammed <fromlatonyy@gmail.com>
2023-02-17 03:11:36 -0800

    er

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,7 +13,7 @@ int main(void)
 	int i;
 	char c[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-	for (i = 0; i < 27; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(c[i]);
 	}
