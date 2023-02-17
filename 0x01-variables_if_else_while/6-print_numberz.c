b8dfd43e11719c21cc0a09c240c15172afad6bb2
Mohammed <fromlatonyy@gmail.com>
2023-02-17 04:11:21 -0800

    er

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints the alphabet
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
