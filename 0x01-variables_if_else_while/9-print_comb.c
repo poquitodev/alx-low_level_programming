42c70b1ad629816bce4f8c9f111bde85dbdfd2e3
Mohammed <fromlatonyy@gmail.com>
2023-02-17 04:38:44 -0800

    er

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,14 +13,15 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 48; j < 58; j++)
 		{
 		putchar(i);
 		putchar(j);
+		putchar(',');
+		putchar(' ');
 		}
 	}
-	putchar('\n');
 	return (0);
 }
