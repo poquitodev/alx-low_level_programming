8072ccc0f8924acc883c39828dd478c7fcf0f1c0
Mohammed <fromlatonyy@gmail.com>
2023-02-26 22:54:17 -0800

    gt

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - check the code
+ *
+ * @s: a pointer to a string
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+
+	for (int i = 0; *s[i] != '\0'; i++)
+	{
+
+	}
+
+return (i);
+}
