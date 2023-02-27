37d15bda92667b067f34e97fb2ff9cfc08d70aab
Mohammed <fromlatonyy@gmail.com>
2023-02-26 22:38:59 -0800

    gt

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ *
+ * @a: pointer to an int
+ *
+ * @b: pointer to another int
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+
+	int c;
+
+	c = *a;
+
+	*a = *b;
+
+	*b = c;
+
+}
