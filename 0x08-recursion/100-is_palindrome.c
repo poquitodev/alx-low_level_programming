79efd703c4ddc16193c98fd3a8a5a6c193493d47
Mohammed <fromlatonyy@gmail.com>
2023-03-08 01:37:57 -0800

    r

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,34 +1,44 @@
 #include "main.h"
 
 /**
- * is_palindrome_helper - helper function
- *
- * @s: pointer to string params
- * @end: used to find the end of the string
- *
- * Return: recursion
+ * _strlen_recursion - returns the length of a string.
+ * @s: string
+ * Return: the length of a string.
  */
-int is_palindrome_helper(char *s, char *end)
+int _strlen_recursion(char *s)
 {
-	if (s >= end)
-		return (1);
-	if (*s != *end)
+	if (*s == '\0')
 		return (0);
-	return (is_palindrome_helper(s + 1, end - 1));
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
+
 /**
- * is_palindrome - check if the string is a palindrome
- *
- * @s: pointer to string
- *
- * Return: returns 1 if a string is a palindrome and 0 if not
+ * comparator - compares each character of the string.
+ * @s: string
+ * @n1: smallest iterator.
+ * @n2: biggest iterator.
+ * Return: .
+ */
+int comparator(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - detects if a string is a palindrome.
+ * @s: string.
+ * Return: 1 if s is a palindrome, 0 if not.
  */
 int is_palindrome(char *s)
 {
-	char *end = s;
-
-	while (*end)
-		end++;
-	end--;
-	return (is_palindrome_helper(s, end));
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
